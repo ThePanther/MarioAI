f1f8be09eb8036f026547fb471ab462ec7867cbf
sawa112 <AlexanderSawadski@gmail.com>
2014-10-05 18:37:48 +0200

    1. Commit
    Main Datum (3)

--- src/ch/idsia/scenarios/Main.java
+++ src/ch/idsia/scenarios/Main.java
@@ -34,7 +34,7 @@ import ch.idsia.tools.MarioAIOptions;
  * Created by IntelliJ IDEA. User: Sergey Karakovskiy, sergey at idsia dot ch Date: Mar 17, 2010 Time: 8:28:00 AM
  * Package: ch.idsia.scenarios
  * 
- * 30.09.2014 (2) 
+ * 30.09.2014 (3)
  */
 public final class Main
 {
