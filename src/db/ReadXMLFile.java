1005983f8d2576b5308ce80a8ddbe807bf64720e
timotin <w.timon@gmail.com>
2014-10-15 15:52:20 +0200

    xml datei lesen + jconnector

---
+++ src/db/ReadXMLFile.java
@@ -0,0 +1,53 @@
+package db;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+ 
+public class ReadXMLFile {
+ 
+  public static void main(String argv[]) {
+ 
+    try {
+ 
+	File fXmlFile = new File("config/db.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+ 
+	//optional, but recommended
+	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+	doc.getDocumentElement().normalize();
+ 
+	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+ 
+	NodeList nList = doc.getElementsByTagName("datenbank");
+ 
+	System.out.println("----------------------------");
+ 
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+ 
+		Node nNode = nList.item(temp);
+ 
+		//System.out.println("\nCurrent Element :" + nNode.getNodeName());
+ 
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+ 
+			Element eElement = (Element) nNode;
+			System.out.println("Host : " + eElement.getElementsByTagName("host").item(0).getTextContent());
+			System.out.println("DBname : " + eElement.getElementsByTagName("dbname").item(0).getTextContent());
+			System.out.println("Username : " + eElement.getElementsByTagName("username").item(0).getTextContent());
+			System.out.println("Password : " + eElement.getElementsByTagName("password").item(0).getTextContent());
+ 
+		}
+	}
+    } catch (Exception e) {
+	e.printStackTrace();
+    }
+  }
+ 
+}
