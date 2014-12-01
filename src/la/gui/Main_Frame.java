febb2275bcaec5ef31c15aefa164a8e1452c048d
RubenChristianBuhl <rubenchristian.buhl@haw-hamburg.de>
2014-12-01 10:33:41 +0100

    Textfelder für Rewards initialisiert

--- src/la/gui/Main_Frame.java
+++ src/la/gui/Main_Frame.java
@@ -281,15 +281,15 @@ public class Main_Frame {
         episodesTextField.setText("10");
         seedTextField.setText("0");
         fpsTextField.setText("1000");
-        winTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(winTextField.getName()).getReward()));
-        lossTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(lossTextField.getName()).getReward()));
-        hurtTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(hurtTextField.getName()).getReward()));
-        rightTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(rightTextField.getName()).getReward()));
-        leftTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(leftTextField.getName()).getReward()));
-        stompTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(stompTextField.getName()).getReward()));
-        frameTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(frameTextField.getName()).getReward()));
-        upTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(upTextField.getName()).getReward()));
-        downTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(downTextField.getName()).getReward()));
+        winTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(winLable.getText()).getReward()));
+        lossTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(lossLable.getText()).getReward()));
+        hurtTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(hurtLable.getText()).getReward()));
+        stompTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(stompLable.getText()).getReward()));
+        frameTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(frameLable.getText()).getReward()));
+        rightTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(rightLable.getText()).getReward()));
+        leftTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(leftLable.getText()).getReward()));
+        upTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(upLable.getText()).getReward()));
+        downTextField.setText(String.valueOf(rlGlueService.getCurrentReward().getReward(downLable.getText()).getReward()));
 
         db = ManagerFactory.getManager(Database.class);
 
